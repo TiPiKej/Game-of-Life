d70c6aeed11c50bb7266a770fba1bbaf9510edbf
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-10-12 21:44:55 +0200

    .

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,7 +1,17 @@
 package life;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final Scanner scanner = new Scanner(System.in);
+
+        final int N = scanner.nextInt();
+        final int S = scanner.nextInt();
+
+        scanner.close();
+
+        Game game = new Game(N, S);
+        System.out.println(game.toString());
     }
 }
